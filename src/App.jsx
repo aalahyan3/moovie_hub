887228537135552388788cb11a579a8fda2aea7d
Ayoub Alahyane <aalahyan@e3r3p9.1337.ma>
2025-02-20 17:54:39 +0100

    some enhacements

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
       <Route path='/details/:id' element={<MovieDetails />}/>
       <Route path='/search' element={<Search />}/>
     </Routes>
+      <Analytics />
     </BrowserRouter>
   )
 }

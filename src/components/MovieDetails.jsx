920292992ded677a649eb0523cb15378baa81ced
Ayoub Alahyane <aalahyan@e3r3p9.1337.ma>
2025-02-20 17:30:07 +0100

    fixed some issues

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -90,7 +90,8 @@ const MovieDetails = ()=>
 				release_date={movie.release_date}
 				duration={movie.runtime}
 				/>
-				{movie.production_companies && movie.production_companies.lenght > 0 && <ProductionCompanies comps={movie.production_companies}/>}
+				{console.log(movie.production_companies)}
+				{movie.production_companies && movie.production_companies.length > 0 &&  <ProductionCompanies comps={movie.production_companies}/>}
 				<Trailer id={id} />
 				<Recomendations id={id}/>
 			</div>

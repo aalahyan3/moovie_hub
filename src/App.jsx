6ea5973a3f64eb637d97b7ac03bf841c9fb01da9
Ayoub Alahyane <aalahyan@e3r3p9.1337.ma>
2025-02-20 17:51:17 +0100

    some enhacements

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Loader from './components/Loader'
 import MovieDetails from './components/MovieDetails'
 import Search from './components/Search'
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
 
   return (
